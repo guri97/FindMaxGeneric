fc20c84f0aacf87d0826b3cedfd454590d1ab090
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 20:59:05 +0530

    [gurpreet] UC3_FindMaxStringNumber

--- FindMaxGeneric/FindMaxGeneric/Program.cs
+++ FindMaxGeneric/FindMaxGeneric/Program.cs
@@ -10,6 +10,8 @@
             Console.WriteLine(integers);
             double Double = MaxNumberCheck.MaxFloatNumber(1.2, 3.4, 5.6);
             Console.WriteLine(Double);
+            string string1 = MaxNumberCheck.MaximumString("22", "23", "26");
+            Console.WriteLine(string1);
 
 
         }
