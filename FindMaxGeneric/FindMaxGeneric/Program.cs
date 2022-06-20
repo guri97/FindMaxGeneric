fe50c0f33239805b82b9505876112dab59f02e69
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 21:23:53 +0530

    [gurpreet] UC3_Refactor1GenericClass

--- FindMaxGeneric/FindMaxGeneric/Program.cs
+++ FindMaxGeneric/FindMaxGeneric/Program.cs
@@ -6,17 +6,21 @@
         {
             Console.WriteLine("Welcome to Maximum value finder");
             // MaxNumberCheck maxNumberCheck = new MaxNumberCheck();
+
             int integers = MaxNumberCheck.MaxIntegerNumber(4, 6, 2);
             Console.WriteLine(integers);
             double Double = MaxNumberCheck.MaxFloatNumber(1.2, 3.4, 5.6);
             Console.WriteLine(Double);
             string string1 = MaxNumberCheck.MaximumString("22", "23", "26");
             Console.WriteLine(string1);
+            Console.WriteLine("Generic Method and Class");
+            int max = GenericMaximum<int>.MaxValue(4, 6, 2);
+            Console.WriteLine(max);
+            double max1 = GenericMaximum<double>.MaxValue(1.2, 3.4, 5.6);
+            Console.WriteLine(max1);
+            string max2 = GenericMaximum<string>.MaxValue("22", "23", "26");
+            Console.WriteLine(max2);
 
-            Console.WriteLine("generic method");
-            GenericMethod.GetMaximum<int>(4, 6, 2);
-            GenericMethod.GetMaximum<double>(1.2, 3.4, 5.6);
-            GenericMethod.GetMaximum<string>("22", "23", "26");
 
 
 
