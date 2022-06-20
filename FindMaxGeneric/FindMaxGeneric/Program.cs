c0eec153ff52da307a30cf481abf693877cb624d
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-20 21:49:41 +0530

    [gurpreet] UC4_SortingUsingMax

--- FindMaxGeneric/FindMaxGeneric/Program.cs
+++ FindMaxGeneric/FindMaxGeneric/Program.cs
@@ -5,7 +5,6 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Maximum value finder");
-            // MaxNumberCheck maxNumberCheck = new MaxNumberCheck();
 
             int integers = MaxNumberCheck.MaxIntegerNumber(4, 6, 2);
             Console.WriteLine(integers);
@@ -20,6 +19,16 @@
             Console.WriteLine(max1);
             string max2 = GenericMaximum<string>.MaxValue("22", "23", "26");
             Console.WriteLine(max2);
+            Console.WriteLine("Generic Method and Class using sorting method");
+            int[] arr = { 4, 6, 2 };
+            GenericMaximumNumber<int> generic = new GenericMaximumNumber<int>(arr);
+            generic.PrintMaxValue();
+            double[] arr1 = { 1.2, 3.4, 5.6 };
+            GenericMaximumNumber<double> generic1 = new GenericMaximumNumber<double>(arr1);
+            generic1.PrintMaxValue();
+            string[] arr2 = { "22", "23", "26" };
+            GenericMaximumNumber<string> generic2 = new GenericMaximumNumber<string>(arr2);
+            generic2.PrintMaxValue();
 
 
 
